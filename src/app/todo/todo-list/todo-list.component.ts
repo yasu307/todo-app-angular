84dc7a4b4472b3e5c045bd1cb09c9f26435feae3
Yusuke Yasuoka <yusuke.yasuoka@nextbeat.net>
2022-06-15 19:30:14 +0900

    create todo list view
    todo一覧画面の作成
    todo-itemに編集、削除ボタンの追加はまだ行っていない
    colorをRGBに変換する処理をパイプとして作成

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -11,17 +11,22 @@ import { CategoryService } from 'src/app/category/category.service';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-  todos$?: Observable<Todo[]>
+  todos$?:      Observable<Todo[]>
   categories$?: Observable<Category[]>
 
   constructor(
-    private todoService: TodoService,
+    private todoService:     TodoService,
     private categoryService: CategoryService
   ) { }
 
   ngOnInit(): void {
-    this.todos$ = this.todoService.getTodos()
+    this.todos$      = this.todoService.getTodos()
     this.categories$ = this.categoryService.getCategories()
+  }
+
+  // idから対応するカテゴリを取得するメソッド
+  getCategoryById(id: number, categories: Category[]| null): Category | undefined {
+    return categories?.find((c: Category) => c.id == id)
   }
 
 }
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'
+import { PipeModule } from '../pipe/pipe.module';
 
 @NgModule({
   declarations: [
     TodoListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatListModule,
+    PipeModule,
   ]
 })
 export class TodoModule { }
