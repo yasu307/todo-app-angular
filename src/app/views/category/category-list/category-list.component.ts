2af894c7d71a9911581e501fadb8b88f7e51bdd7
Yusuke Yasuoka <yusuke.yasuoka@nextbeat.net>
2022-06-21 17:57:00 +0900

    Create category list view

--- src/app/views/category/category-list/category-list.component.ts
+++ src/app/views/category/category-list/category-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from 'src/app/models/category';
 import { CategoryService } from '../category.service';
+import { faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { faPlusCircle, faCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-category-list',
@@ -11,10 +13,31 @@ import { CategoryService } from '../category.service';
 export class CategoryListComponent implements OnInit {
   allCategory$: Observable<Category[]> = this.categoryService.allCategory$
 
+  faEdit       = faEdit
+  faTrashAlt   = faTrashAlt
+  faPlusCircle = faPlusCircle
+  faCircle     = faCircle
+
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.categoryService.fetchAllCategory()
   }
 
+  // カテゴリ更新ダイアログを表示する
+  showEditDialog(category: Category) {
+    console.log("show edit dialog")
+    // const editDialogRef = this.dialog.open(TodoFormDialogComponent, { data: todo, width: '700px' })
+  }
+
+  deleteComponent(categoryId: number) {
+    console.log("delete component")
+    // this.categoryService.deleteTodo(categoryId).subscribe()
+  }
+
+  // カテゴリ追加ダイアログを表示する
+  showStoreDialog() {
+    console.log("show store dialog")
+    // const storeDialogRef = this.dialog.open(TodoFormDialogComponent, { width: '700px' })
+  }
 }
--- src/app/views/category/category.module.ts
+++ src/app/views/category/category.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryListComponent } from './category-list/category-list.component';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PipeModule } from '../../pipe/pipe.module';
 
 @NgModule({
   declarations: [
     CategoryListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    FontAwesomeModule,
+    PipeModule,
   ]
 })
 export class CategoryModule { }
